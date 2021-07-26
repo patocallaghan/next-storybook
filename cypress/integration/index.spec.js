08f1703e8c2b964ebcdd284ae48dced5ac4d6a66
Pat O'Callaghan <pat@intercom.io>
2021-07-26 14:43:28 +0100

    Add cypress and storybook integration

--- cypress/integration/index.spec.js
+++ cypress/integration/index.spec.js
@@ -1,8 +1,9 @@
 /// <reference types="cypress" />
 
-describe("example to-do app", () => {
-  it("displays two todo items by default", () => {
+describe("Homepage", () => {
+  it("works", () => {
     cy.visit("http://localhost:3000");
     cy.get("h1").should("have.text", "Next.js + Storybook + Percy");
+    cy.percySnapshot("Homepage");
   });
 });
