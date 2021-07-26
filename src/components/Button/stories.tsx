48e1353fce81997e3b400b188eaba00eb4d8bb10
Pat O'Callaghan <pat@intercom.io>
2021-07-26 12:14:45 +0100

    Add in-percy

--- src/components/Button/stories.tsx
+++ src/components/Button/stories.tsx
@@ -1,17 +1,25 @@
 import React from "react";
-import { Button } from "./index";
+import { Button, IProps } from "./index";
+import inPercy from "@percy-io/in-percy";
 
 export default {
   title: "Button",
 };
 
-export const DefaultButton = () => (
+export const DefaultButton = (args: IProps) => (
   <div style={{ padding: "25px" }}>
-    <Button size="small" label="My Button" classNames="" type="primary" />
+    <Button {...args} />
   </div>
 );
 
 DefaultButton.story = {
   decorators: [],
   parameters: {},
+};
+
+DefaultButton.args = {
+  size: "small",
+  label: "My Button",
+  classNames: inPercy() ? "storybook-button--percy" : "",
+  type: "primary",
 };
